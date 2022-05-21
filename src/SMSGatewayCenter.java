fbfb3f1be57a88fe52e2496f3949a0a1ee53cb4c
Parth Shah <codersage.in@gmail.com>
2022-05-21 18:38:12 +0530

    Created sendOTP function

---
+++ src/SMSGatewayCenter.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Random;
+
+
+public class SMSGatewayCenter{
+
+    public static void main(String args[]){
+        Random rnd = new Random();
+        int number = rnd.nextInt(9999);
+
+        // this will convert any number sequence into 6 character.
+        System.out.printf( "%04d", number);
+//        System.out.println(sendOTP("VDJ1muqjMtm6puO4",
+//                "is your One Time Password(OTP) for Merit Access. This OTP is valid till 120 sec - CHARUSAT",
+//                "CHRUST",
+//                "8320847007"));
+    }
+
+    public static String sendOTP(String api, String msg, String snd, String num) {
+        try {
+            // Construct data
+
+            String apiKey = "apikey=" + api;
+            String message = "&message=" + msg;
+            String sender = "&senderid=" + snd;
+            String numbers = "&number=" + num;
+
+//            String apiKey = "apikey=" + "VDJ1muqjMtm6puO4";
+//            String message = "&message=" + "is your One Time Password(OTP) for Merit Access. This OTP is valid till 120 sec - CHARUSAT";
+//            String sender = "&senderid=" + "CHRUST";
+//            String numbers = "&number=" + "9767080185";
+
+            // Send data
+            HttpURLConnection conn = (HttpURLConnection) new URL("https://login.smsforyou.biz/V2/http-api.php?").openConnection();
+            String data = apiKey + numbers + message + sender;
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+            conn.getOutputStream().write(data.getBytes("UTF-8"));
+            final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            final StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                stringBuffer.append(line);
+            }
+            rd.close();
+
+            return stringBuffer.toString();
+        } catch (Exception e) {
+            System.out.println("Error SMS "+e);
+            return "Error "+e;
+        }
+    }
+}
